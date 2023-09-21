c898cc315a0b036dbbd7850d1d29be0df26ed581
Simendeeva Liza <liza.simendeeva@mail.ru>
2023-09-21 13:00:18 +0700

    Version with describe class

--- new_project.cs
+++ new_project.cs
@@ -1,1 +1,14 @@
 using System;
+
+namespace ConsoleApp1 {
+ class Program {
+  delegate double Function (double x);
+  static void Table (double x1, double dx, double x2, Function f) {
+   double y;
+   string OutputFormat = "{0:F4}	{1:F4}";
+   Console.WriteLine (OutputFormat, "x", "y");
+   for (double x = x1; x <= x2; x += dx) {
+    y = f (x);
+    Console.WriteLine (OutputFormat,x,y);
+   }
+  }
